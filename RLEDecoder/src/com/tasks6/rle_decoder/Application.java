30455edaa937a61f93c854257896a278b563898c
BohdanKoba <bohdankoba97@gmail.com>
2020-08-13 00:45:29 +0300

    друга спроба

--- RLEDecoder/src/com/tasks6/rle_decoder/Application.java
+++ RLEDecoder/src/com/tasks6/rle_decoder/Application.java
@@ -2,6 +2,7 @@ package com.tasks6.rle_decoder;
 
 public class Application {
 	public static void main(String[] args) {
+		
 		if (args[0].equals("") || Character.isDigit(args[0].charAt(0))) {
 			System.out.println("");
 		}
@@ -12,15 +13,30 @@ public class Application {
 				char symbol = args[0].charAt(i);
 				if (Character.isDigit(args[0].charAt(i)) && Character.isDigit(args[0].charAt(i - 1))) {
 					System.out.println("");
+					return;
 				}
-				if (!Character.isDigit(args[0].charAt(i)) && Character.isDigit(args[0].charAt(i - 1))) {
-					for (int j = 0; j < Character.getNumericValue(args[0].charAt(i - 1)); j++) {
+				if (args[0].charAt(i) == args[0].charAt(i - 1)) {
+					System.out.println("");
+					return;
+				}
+				if (!Character.isDigit(args[0].charAt(i))) {
+					if (i == 1) {
+						s.append(args[0].charAt(0)).append(symbol);
+					}
+					if (i != args[0].length() - 1 && Character.isDigit(args[0].charAt(i + 1))) {
+						continue;
+					}
+					else {
 						s.append(symbol);
 					}
-					if ()
+				}
+				if (Character.isDigit(args[0].charAt(i))) {
+					for (int j = 0; j < Character.getNumericValue(args[0].charAt(i)); j++) {
+						s.append(args[0].charAt(i - 1));
+					}
 				}
 			}
-			System.out.println(s.toString() + " decoded");
+			System.out.println(s.toString());
 		}
 	}
 }
--- RLEDecoder/src/com/tasks6/rle_decoder/Test.java
+++ RLEDecoder/src/com/tasks6/rle_decoder/Test.java
@@ -8,5 +8,11 @@ public class Test {
 	    Application.main(new String [] {"ababasd adsadasd Adad"});	// ababasd adsadasd Adad
 	    Application.main(new String [] {"C2ap2ucin2o9o"});	// CCappucinnoooooooooo
 	    Application.main(new String [] {"Hh9h4e4l4o"});	// Hhhhhhhhhhhhhheeeellllo
+
+	    Application.main(new String[]{"d2f0"});
+	    Application.main(new String[]{"Hh9h4e4l2o44"});
+	    Application.main(new String[]{"H22l2L2o"});
+	    Application.main(new String[]{"H2ee2l2L2o2"});
+	    Application.main(new String[]{"H2ee2l2L2oo"});
 	}
 }
